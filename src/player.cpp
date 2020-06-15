5aa3dd8a2a55f91d74d8c6ce05e00a60e8959f9b
watasuke102 <watasuke102@gmail.com>
2020-06-15 15:06:48 +0900

    Update: 当たり判定部分をすべて削除

--- src/player.cpp
+++ src/player.cpp
@@ -91,50 +91,15 @@ void _player::move()
 	}
 
 	pos.y -= speed.y;
-
-	/**
-	if (KeyUp.pressed())   pos.y -= 5;
-	if (KeyDown.pressed()) pos.y += 5;
 	map->scr.x -= speed.x;
-	return;
-	/**/
 
 	//重力
 	speed.y--;
-	/**
 	if (pos.y >= 900-hitBox.size.y)
 	{
 		jumpCnt = 0;
 		pos.y = 900-hitBox.size.y;
 		speed.y = 0;
 	}
-	/**/
-	//当たり判定
-	_mapHitState hit = map->checkMapHitState(pos-map->scr, speed, hitBox);
 
-	Print << U"top:{},bottom:{},right:{},left:{}"_fmt(hit.top, hit.bottom, hit.right, hit.left);
-
-	if(hit.bottom)
-		jumpCnt = 0;
-	if(hit.top || hit.bottom)
-	{
-		speed.y = 0;
-	}
-	if(hit.left || hit.right)
-	{
-		//speed.x =/**/ hit.pos.x - (pos.x - map->scr.x);
-		//debug = U"speed.xはhit.pos.x[{}] - (pos.x[{}] - map->scr.x[{}])の結果{}となりました"_fmt(hit.pos.x, pos.x, map->scr.x, speed.x);
-		/*/pos.x-(hit.pos.x + map->scr.x);
-		/**/
-		speed.x = map->scr.x - (pos.x - hit.pos.x);
-		debug = U"speed.x = map->scr.x[{}] - (pos.x[{}]-hit.pos.x[{}]) = {}"_fmt(map->scr.x, pos.x, hit.pos.x, speed.x);
-		dashingTime = 20;//次のupdate()でダッシュを終了させる
-	}
-	//当たり判定終わり
-
-	//移動
-	Print << U"hit{} player{}"_fmt(hit.pos, pos-map->scr);
-	Print << U"playerSpeed" << speed;
-	pos.y = hit.pos.y;
-	map->scr.x -= speed.x;
 }
